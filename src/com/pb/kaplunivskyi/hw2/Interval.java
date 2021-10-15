9a28944088f0f3d05ba0736eca8b401550021ebf
Yurii Kaplunivskyi <yurii.kaplunivskyi@gmail.com>
2021-10-15 22:28:29 +0300

    init commit

--- src/com/pb/kaplunivskyi/hw2/Interval.java
+++ src/com/pb/kaplunivskyi/hw2/Interval.java
@@ -7,7 +7,7 @@ public class Interval {
         int x;
         System.out.println("Введите числа из диапазона от 0 до 100");
         x = scan.nextInt();
-        if (x == 0 || x < 101) {
+        if (x == 0 || x < 101)  {
             if (x <= 14) {
                 System.out.print("диапазон[0-14]");
             } else if (x >= 15 && x <= 35) {
@@ -17,6 +17,8 @@ public class Interval {
             } else if (x >= 51 && x <= 100) {
                 System.out.print("диапазон[51-100]");
             }
+        }else if (x>=101) {
+            System.out.print("Error.Написано же от 0 до 100");
         }
 
     }
