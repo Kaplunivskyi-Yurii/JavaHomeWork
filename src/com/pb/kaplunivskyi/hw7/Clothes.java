6919fdd3c3fbcbd48f615323e8d84f4ab259969b
Yurii Kaplunivskyi <yurii.kaplunivskyi@gmail.com>
2021-11-15 19:46:21 +0200

    init commit

---
+++ src/com/pb/kaplunivskyi/hw7/Clothes.java
@@ -0,0 +1,13 @@
+package com.pb.kaplunivskyi.hw7;
+
+public abstract class Clothes implements ManClothes, WomenClothes {
+    enum Size{
+
+        XXS,
+        XS,
+        S,
+        M,
+        L
+    }
+}
+
---
+++ src/com/pb/kaplunivskyi/hw7/ManClothes.java
@@ -0,0 +1,5 @@
+package com.pb.kaplunivskyi.hw7;
+
+public interface ManClothes {
+    void dressMan();
+}
