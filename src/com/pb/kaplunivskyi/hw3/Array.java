a472bca141a66be6b4d4a207d7bdb27df2640112
Yurii Kaplunivskyi <yurii.kaplunivskyi@gmail.com>
2021-11-20 18:38:44 +0200

    init commit

--- src/com/pb/kaplunivskyi/hw3/Array.java
+++ src/com/pb/kaplunivskyi/hw3/Array.java
@@ -1,30 +1,48 @@
 package com.pb.kaplunivskyi.hw3;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[] array = new int[10];
-        System.out.println("Вставить элементы массива: ");
+        Scanner scanner = new Scanner(System.in);
 
-        for(int i = 0; i < 10; i++){
-            array[i] = scan.nextInt();
-        }
-        System.out.print("Вывести на экран введенный массив:");
-        for(int i = 0; i < 10; i++) {
-            System.out.print ("" + array[i]);
-        }
-        int sum = 0;
-        for (int i: array){
-            sum = sum + array[i];
-            System.out.println(" Cумма элементов массива равна:" + sum);}
-            int counter = 0;
-            for(int i = 0; i < 10; i++)
-            {
-                if(array[i] > 0)
-                    counter += 1;
-                System.out.println("кол-во положительных" + counter);
-                break;
+        int[] numbersArr = new int[10];
+        int counter = 0;
+        while (counter < numbersArr.length) {
+            System.out.println("Enter any integer (entered " + (counter + 1) + " of " + numbersArr.length + "): ");
+            if (scanner.hasNextInt()) {
+                numbersArr[counter] = scanner.nextInt();
+                counter++;
+            } else {
+                scanner.next();
+                System.out.println("You entered a non-integer number!");
             }
+        }
+
+        int arraySum = 0;
+        int positiveNumbers = 0;
+        for (int i = 0; i < numbersArr.length; i++) {
+            arraySum += numbersArr[i];
+            if (numbersArr[i] > 0) {
+                positiveNumbers++;
             }
+        }
+
+        System.out.println("Your array: " + Arrays.toString(numbersArr));
+        System.out.println("Sum of array elements: " + arraySum);
+        System.out.println("Number of positive elements: " + positiveNumbers);
+
+        for (int i = 0; i < numbersArr.length; i++) {
+            for (int j = 0; j < numbersArr.length - 1; j++) {
+                if (numbersArr[j] > numbersArr[j + 1]) {
+                    int number = numbersArr[j];
+                    numbersArr[j] = numbersArr[j + 1];
+                    numbersArr[j + 1] = number;
+                }
+            }
+        }
+
+        System.out.println("Your array: " + Arrays.toString(numbersArr));
     }
+}
