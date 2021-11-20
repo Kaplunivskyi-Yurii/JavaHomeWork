79170d01ce3802c2dd06198c4cefce8e2f881d68
Yurii Kaplunivskyi <yurii.kaplunivskyi@gmail.com>
2021-11-20 18:31:46 +0200

    init commit

---
+++ src/com/pb/kaplunivskyi/hw8/WrongLoginException.java
@@ -0,0 +1,8 @@
+package com.pb.kaplunivskyi.hw8;
+
+public class WrongLoginException extends Exception {
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/pb/kaplunivskyi/hw8/WrongPasswordException.java
@@ -0,0 +1,4 @@
+package com.pb.kaplunivskyi.hw8;
+
+public class WrongPasswordException extends Exception {
+}
